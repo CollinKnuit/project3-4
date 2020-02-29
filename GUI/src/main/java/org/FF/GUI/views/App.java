e449b331688b62c179dad28f1e699a09aa5ae538
CollinKnuit <collinkuit@gmail.com>
2020-03-01 00:37:21 +0100

    Added basic project structure
    - added the view package for the views
    - created the App class
    - added the common package for normal java classes
    - added the resources package for external resources 
    - Updated the jserialComm dependency
    - Added a fun theme dependency
    - Added a Helpfull sources section in the readme

---
+++ GUI/src/main/java/org/FF/GUI/views/App.java
@@ -0,0 +1,54 @@
+package org.FF.GUI.views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+public class App {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					App window = new App();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public App() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 434, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 261, Short.MAX_VALUE)
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+}
--- README.md
+++ README.md
@@ -4,7 +4,17 @@ A very awsome ATM
 Name of bank: Flasche Flocken 
 initials: FF
 
+
 Code conventions
 1) All comments are in English
 2) Begining curly braces are not on a newline
 3) If you add a new feature or edit exsisting feature in dev make a new branch for that
+
+Helpfull sources:
+1) 	Playlist to create a GUI with the Eclipse window builder
+	https://www.youtube.com/watch?v=E7Y6Ng0LEGk&list=PLGpGtmwCknzA3WHmQ7MSx7VP0mRdQXPK1
+	
+2)	A possible way to create multiple views
+	https://stackoverflow.com/questions/5077321/how-could-i-make-the-jframe-content-change-to-corresponding-click
+
+
