7a118309ea2c7220d3cee06b997f6972b2caa242
Frank Montenij <frankmonteny02@gmail.com>
2020-03-26 14:06:21 +0100

    connection met de keypad toegevoegd zodat er van 1 scherm naar het andere gegaan kan worden via de keypad. (werkt nog niet er komt geen plaatje tevoorschijn)

--- GUI/src/main/java/org/FF/GUI/views/Painter.java
+++ GUI/src/main/java/org/FF/GUI/views/Painter.java
@@ -1,12 +1,84 @@
 package org.FF.GUI.views;
-import java.awt.*;  
-import javax.swing.*;  
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import org.FF.GUI.common.ComPortSetup;
+import org.FF.GUI.common.SerialConnection;  
 
 public class Painter {
 	
 	private JFrame f;
 	private Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
 	private JLayeredPane p = new JLayeredPane();
+	private static ArrayList<SerialConnection> serialConnection = new ComPortSetup().setupComPort(); // gets all comm ports
+	private Timer timer;
+	private String img = "";
+	
+	private ActionListener keypadSwitchScreenListener = new ActionListener() {
+		
+		private String img;
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// gets the input from the keypad
+			var commands = serialConnection.get(0).getWaitingQueue();
+			System.out.println(this.img);
+			// for each input loop it
+			for(var c: commands) {	
+				
+				switch(c) {
+				  
+				  case "A":
+					  if(this.img.contentEquals("FH1_1")) {
+						  switchPane("FB1_1");
+					  } else if(this.img.contentEquals("FP1_1") || this.img.contentEquals("FS1_1") || this.img.contentEquals("FV1_1")) {
+						  switchPane("FH1_1");
+					  }
+					  break;
+				  case "B":
+					  if(this.img.contentEquals("FH1_1")) {
+						  switchPane("FP1_1");
+					  } else if(this.img.contentEquals("FP1_1")) {
+						  switchPane("FV1_1");
+					  } else if(this.img.contentEquals("FV1_1")) {
+						  switchPane("FP1_1");
+					  } else if(this.img.contentEquals("FS1_1")) {
+						  switchPane("FP_1");
+					  }
+					  break;
+				  case "C":
+					  if(this.img.contentEquals("FH1_1")) {
+						  switchPane("FS1_1");
+					  }
+					  break;
+				  case "D":
+					  if(this.img.contentEquals("FH1_1")) {
+						  switchPane("FW1_1");
+					  } else if(this.img.contentEquals("FP1_1") || this.img.contentEquals("FS1_1") || this.img.contentEquals("FV1_1")) {
+						  switchPane("FH1_1");
+					  }
+					  break;
+				  case "*":
+					  break;
+				  case "#":
+					  break;
+				  
+				  default:
+					  break;
+					 
+				}
+			}
+		}
+		
+		public void setImg(String img) {
+			this.img = img;
+		}
+	};
 	
 	Painter(String img){ 
 		/* instantiate the JFrame, the background image 
@@ -18,11 +90,20 @@ public class Painter {
 		this.f = new JFrame();
 		ImageIcon bgIcon = new ImageIcon(this.getClass().getResource("/resources/" + img + ".png"));
 		JLabel l = new JLabel("", bgIcon, SwingConstants.CENTER);
+		JTextField listenerObject = new JTextField();
+		
+		listenerObject.setBounds(0, 0, 0, 0);
+		listenerObject.setVisible(false);
+		listenerObject.addActionListener(keypadSwitchScreenListener);
+		timer = new Timer(10, keypadSwitchScreenListener);
+		timer.start();
+		
 		this.f.setBounds(0, 0, this.screensize.width, this.screensize.height);
 		this.p.setBounds(0, 0, this.screensize.width, this.screensize.height);
 		l.setBounds(0, 0, this.screensize.width, this.screensize.height);
+		
 		this.p.add(l);
-		if(img.equals("FS1_1")) {
+		if(img.contentEquals("FS1_1")) {
 			JTextField saldo = new JTextField();
 			saldo.setBounds(690, 470, 350, 60);
 			saldo.setColumns(10);
@@ -30,15 +111,16 @@ public class Painter {
 			this.p.add(saldo);
 		}
 		
-		if(img.equals("FV1_1")) {
+		if(img.contentEquals("FV1_1")) {
 			JTextField saldo = new JTextField();
 			saldo.setBounds(770, 460, 350, 60);
 			saldo.setColumns(10);
 			saldo.setEditable(false);
 			this.p.add(saldo);
 		}
-		
+		this.p.add(listenerObject);
 		this.f.add(p);
+		
 		this.f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.f.getContentPane().setLayout(null);
 		this.f.setUndecorated(true);
@@ -52,10 +134,20 @@ public class Painter {
 		 */
 		JLayeredPane p2 = new JLayeredPane();
 		ImageIcon bgIcon = new ImageIcon(this.getClass().getResource("/resources/" + img + ".png"));
-		JLabel l = new JLabel("", bgIcon, SwingConstants.CENTER);
+		JLabel imageLabel = new JLabel("", bgIcon, SwingConstants.CENTER);
+		JTextField listenerObject = new JTextField();
+		
+		listenerObject.setBounds(0, 0, 0, 0);
+		listenerObject.setVisible(false);
+		listenerObject.addActionListener(keypadSwitchScreenListener);
+		timer = new Timer(10, keypadSwitchScreenListener);
+		timer.start();
+		
 		p2.setBounds(0, 0, this.screensize.width, this.screensize.height);
-		l.setBounds(0, 0, this.screensize.width, this.screensize.height);
-		p2.add(l);
+		imageLabel.setBounds(0, 0, this.screensize.width, this.screensize.height);
+		
+		p2.add(imageLabel);
+		p2.add(listenerObject);
 		
 		if(img.equals("FS1_1")) {
 			JTextField saldo = new JTextField();
