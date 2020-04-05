37cf40d75b24f3d06e2b0179e299d525a9148050
Collin <collinknuit@gmail.com>
2020-04-05 23:11:14 +0200

    Auto stash before revert of "Big update/changes"

---
+++ GUI/src/main/java/org/FF/GUI/common/database/Acount.java
@@ -0,0 +1,36 @@
+package org.FF.GUI.common.database;
+
+import java.math.BigDecimal;
+
+public class Acount {
+
+	private int AcountID;
+	private BigDecimal Balance;
+	private String RfidNumber;
+	
+	
+	public int getAcountID() {
+		return AcountID;
+	}
+	
+	public void setAcountID(int acountID) {
+		AcountID = acountID;
+	}
+	
+	public BigDecimal getBalance() {
+		return Balance;
+	}
+	
+	public void setBalance(BigDecimal balance) {
+		Balance = balance;
+	}
+	
+	public String getRfidNumber() {
+		return RfidNumber;
+	}
+	
+	public void setRfidNumber(String rfidNumber) {
+		RfidNumber = rfidNumber;
+	}
+	
+}
