70f5d1b7148a0b093a953089f7e7e1d79897efd6
Collin <collinknuit@gmail.com>
2020-04-05 23:09:45 +0200

    Big update/changes
    - added the Brypt password class for hash use in the database
    - added the ConnectionClass to get a connection with the database
    - added the DatabaseQueryClass to get query's from the database
    -added the Acount class to locally store the data from the database
    - set all database stuff in a separate package
    - set all comport stuff in a seperate package
    
    Revert "Big update/changes"
    
    This reverts commit df46c2d54a2745b76898d8734adef4dc49fb9d05.
    
    Auto stash before revert of "Big update/changes"

---
+++ GUI/src/main/java/org/FF/GUI/common/database/ConnectionClass.java
@@ -0,0 +1,50 @@
+package org.FF.GUI.common.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+
+	public static Connection conn = null;
+	private String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=Europe/Amsterdam&allowMultiQueries=true";
+	private final String username = "username"; // change to actual userName
+	private final String password = "password"; // change to actual password
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Connection getConnection() {
+		if (conn != null)
+			return conn;
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			return conn;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return conn;
+		}
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean closeConnection() {
+		if (conn == null)
+			return true;
+		try {
+			conn.close();
+			conn = null;
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+}
