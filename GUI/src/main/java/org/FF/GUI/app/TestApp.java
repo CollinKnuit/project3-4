7a025fb027f496f80a56138672396b42f9863188
CollinKnuit <collinkuit@gmail.com>
2020-05-13 12:06:59 +0200

    Test algorithm

---
+++ GUI/src/main/java/org/FF/GUI/app/TestApp.java
@@ -0,0 +1,328 @@
+package org.FF.GUI.app;
+
+public class TestApp {
+
+	private final static int totaal10 = 50;
+	private final static int totaal20 = 50;
+	private final static int totaal50 = 50;
+	private final static int totaal = 250;
+	private final static int hardlimiet = 5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int option = 2;
+		int array[] = { 0, 0, 0 };
+
+		int limiet1 = totaal / 10;
+		if (limiet1 > hardlimiet)
+			limiet1 = hardlimiet;
+		if (limiet1 > totaal10)
+			limiet1 = totaal10;
+
+		int limiet2 = totaal / 20;
+		if (limiet2 > hardlimiet)
+			limiet2 = hardlimiet;
+		if (limiet2 > totaal20)
+			limiet2 = totaal20;
+
+		int limiet3 = totaal / 50;
+		if (limiet3 > hardlimiet)
+			limiet3 = hardlimiet;
+		if (limiet3 > totaal50)
+			limiet3 = totaal50;
+
+		if (totaal > (limiet1 * 10 + limiet2 * 20 + limiet3 * 50))
+			return;
+
+		int rest = -1;
+		int newRest = -1;
+		int biggestNumber = 0;
+
+		if (option == 1) {
+			rest = totaal - limiet1 * 10;
+			array[0] = limiet1;
+			if (rest == 0) {
+				System.out.println(array);
+				return;
+			}
+
+			biggestNumber = biggestNumber(0, totaal20, totaal50);
+
+			if (biggestNumber == 20) {
+				newRest = rest / 20;
+
+				if (newRest < limiet2) {
+					array[1] = newRest;
+					newRest = rest % 20;
+					if (newRest == 10) {
+						array[0] -= 1;
+						array[1] += 1;
+						newRest -= 10;
+					}
+
+					if (newRest == 0) {
+						System.out.println(array);
+						return;
+					}
+				}
+
+				newRest = rest / 50;
+
+				if (newRest < limiet3) {
+
+					array[2] = newRest;
+					newRest = rest % 50;
+
+					switch (newRest) {
+					case 10:
+						if (totaal10 >= array[0] + 1) {
+							array[0] += 1;
+						} else {
+							array[0] -= 1;
+							array[1] += 1;
+						}
+						newRest -= 10;
+						break;
+
+					case 20:
+						array[1] += 1;
+						newRest -= 20;
+						break;
+
+					case 30:
+						if (totaal10 >= array[0] + 1) {
+							array[0] += 1;
+							array[1] += 1;
+						} else {
+							array[0] -= 1;
+							array[1] += 2;
+						}
+						newRest -= 30;
+						break;
+
+					case 40:
+						array[1] += 2;
+						newRest -= 40;
+						break;
+
+					default:
+						// code block
+					}
+				}
+
+				if (newRest == 0) {
+					System.out.println(array);
+					return;
+				}
+
+			} else {
+				newRest = rest / 50;
+
+				if (newRest < limiet3) {
+
+					array[2] = newRest;
+					newRest = rest % 50;
+
+					switch (newRest) {
+					case 10:
+						if (totaal10 >= array[0] + 1) {
+							array[0] += 1;
+						} else {
+							array[0] -= 1;
+							array[1] += 1;
+						}
+						newRest -= 10;
+						break;
+					case 20:
+						array[1] += 1;
+						newRest -= 20;
+						break;
+
+					case 30:
+						if (totaal10 >= array[0] + 1) {
+							array[0] += 1;
+							array[1] += 1;
+						} else {
+							array[0] -= 1;
+							array[1] += 2;
+						}
+
+						newRest -= 30;
+						break;
+					case 40:
+						array[1] += 2;
+						newRest -= 40;
+						break;
+					default:
+						// code block
+					}
+				}
+
+				if (newRest == 0) {
+					System.out.println(array);
+					return;
+				}
+			}
+		}
+
+		else if (option == 2) {
+			rest = totaal - limiet2 * 20;
+			array[1] = limiet2;
+			if (rest == 0) {
+				System.out.println(array);
+				return;
+			}
+			biggestNumber = biggestNumber(totaal10, 0, totaal50);
+
+			if (biggestNumber == 50) {
+				newRest = rest / 50;
+
+				if (newRest < limiet3) {
+
+					array[2] = newRest;
+					rest -= 50*newRest;
+					newRest = rest % 50;
+					
+
+					switch (newRest) {
+					case 10:
+						array[0] += 1;
+
+						newRest -= 10;
+						break;
+					case 20:
+						array[0] += 2;
+						newRest -= 20;
+						break;
+
+					case 30:
+						if (totaal20 >= array[1] + 1) {
+							array[1] += 1;
+							array[0] += 1;
+						} else {
+							array[0] += 3;
+						}
+
+						newRest -= 30;
+						break;
+					case 40:
+						if (totaal20 >= array[1] + 2) {
+							array[1] += 2;
+						}
+
+						if (totaal20 >= array[1] + 1) {
+							array[1] += 1;
+							array[0] += 2;
+						}
+
+						newRest -= 40;
+						break;
+					default:
+						// code block
+					}
+				}
+			} else {
+				newRest = rest / 10;
+
+				if (newRest < limiet1) {
+					array[0] = newRest;
+					newRest = rest % 10;
+
+					rest = newRest * 10;
+				} else {
+					newRest = rest / 50;
+
+					if (newRest < limiet3) {
+
+						array[2] = newRest;
+						newRest = rest % 50;
+
+						switch (newRest) {
+						case 10:
+							array[0] += 1;
+
+							newRest -= 10;
+							break;
+						case 20:
+							array[0] += 2;
+							newRest -= 20;
+							break;
+
+						case 30:
+							if (totaal20 >= array[1] + 1) {
+								array[1] += 1;
+								array[0] += 1;
+							} else {
+								array[0] += 3;
+							}
+
+							newRest -= 30;
+							break;
+						case 40:
+							if (totaal20 >= array[1] + 2) {
+								array[1] += 2;
+							}
+
+							if (totaal20 >= array[1] + 1) {
+								array[1] += 1;
+								array[0] += 2;
+							}
+
+							newRest -= 40;
+							break;
+						default:
+							// code block
+						}
+					}
+				}
+			}
+			
+			if (rest == 0) {
+				System.out.println(array);
+				return;
+			}
+		} else if (option == 3) {
+			rest = totaal - limiet3 * 50;
+			array[2] = limiet3;
+			if (rest == 0) {
+				System.out.println(array);
+				return;
+			}
+			
+			biggestNumber = biggestNumber(totaal10, totaal20, 0);
+			
+			if(biggestNumber == 10) {
+				newRest = rest/10;
+				array[0] = newRest;
+			}else {
+				newRest = rest/20;
+				rest = rest%20;
+				array[1] = newRest;
+				
+				array[0] = rest/10;
+			}
+			
+			if (rest == 0) {
+				System.out.println(array);
+				return;
+			}
+
+		} else if (option == 4) {
+			biggestNumber = biggestNumber(limiet1, limiet2, limiet3);
+		}
+
+	}
+
+	public static int biggestNumber(int a, int b, int c) {
+		if (a > b && a > c) {
+			return 10;
+		} else if (b > a && b > c) {
+			return 20;
+		} else if (c > a && c > b) {
+			return 50;
+		} else {
+			return 50;
+		}
+	}
+
+}
