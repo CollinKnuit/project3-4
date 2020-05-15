79af0ec0f349063db1faaaa1d3168d0ee32c5b71
CollinKnuit <collinkuit@gmail.com>
2020-05-15 19:14:23 +0200

    Working example code serialcommunication arduino

--- ReadASCIIStringtest/ReadASCIIStringtest.ino
+++ ReadASCIIStringtest/ReadASCIIStringtest.ino
@@ -1,8 +1,8 @@
 
 // pins for the LEDs:
 const int redPin = 13;
-
-char ar[5]={0,0,0,0,0};
+const byte size = 5;
+char ar[size];
 
 void setup() {
   // initialize serial:
@@ -10,28 +10,23 @@ void setup() {
   // make the pins outputs:
   pinMode(redPin, OUTPUT);
   
-  while (!Serial) {
-    ;
-  }
+  while (!Serial) { }
+  
 }
 
 void loop() {
   // if there's any serial available, read it:
     if (Serial.available() > 0) {
-      
    
 
-      for (int index = 0; index < 5; index++) {
+      for (int index = 0; index < size; index++) {
     // turn the pin on:
-         ar[index] = Serial.read();
+         Serial.readBytes(ar, size);
       }
       
-      if(ar[0] == '1' && ar[1] == '2' && ar[1] == '2'  && ar[1] == '2'  ){
+      if(ar[0] == '1' && ar[1] == '2' && ar[2] == '3'  && ar[3] == '4' &&  ar[4] == '5' ){
           digitalWrite(redPin, HIGH);
       }
-      delay(500);
-
-
 
     }
  }
