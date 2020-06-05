7b22b205fbad7b52401a35dc6eb0b32ed106fbdd
Achraf Reyani <0980800@hr.nl>
2020-06-05 15:50:48 +0200

    jatoch

---
+++ code_voor_geld_dispenser/code_voor_geld_dispenser.ino
@@ -0,0 +1,88 @@
+char commando[3];
+
+  char brief10;
+  char brief20;
+  char brief50;
+
+void setup() {
+  Serial.begin(9600);
+  commando[0] = 'a';
+  commando[1] = 'a';
+  commando[2] = 'a';
+  pinMode(10, OUTPUT);
+  
+ 
+
+
+  while(!Serial)
+  {
+    
+  }
+}
+
+void stuurMotor10()
+{
+  digitalWrite(10, HIGH);
+  delay(2000);
+  digitalWrite(10, LOW);
+  delay(100);
+}
+
+
+
+void loop() {
+
+  
+
+  if(Serial.available() > 2)
+  {  
+    Serial.readBytes(commando, 3);
+
+    while(Serial.available() > 0)
+    {
+      Serial.read();
+    }
+    
+
+    
+    brief10 = commando[0];
+    brief20 = commando[1];
+    brief50 = commando[2];
+    Serial.print(brief10);
+    Serial.print(brief20);
+    Serial.println(brief50);
+
+    if(brief10 == '1')
+    {
+      stuurMotor10();
+    }
+    if(brief10 == '2')
+    {
+      stuurMotor10();
+      stuurMotor10();
+    }
+    if(brief10 == '3')
+    {
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+    }
+    if(brief10 == '4')
+    {
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+    }
+    if(brief10 == '5')
+    {
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+    }
+  }
+ 
+  
+}
