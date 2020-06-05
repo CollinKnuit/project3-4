f6d12279c1264f012e09de3efb738475060af8eb
Frank Montenij <frankmonteny02@gmail.com>
2020-06-05 20:40:23 +0200

    with help from Collin I fixed code_voor_geld_dispenser

--- code_voor_geld_dispenser/code_voor_geld_dispenser.ino
+++ code_voor_geld_dispenser/code_voor_geld_dispenser.ino
@@ -1,23 +1,18 @@
 char commando[3];
-
-  char brief10;
-  char brief20;
-  char brief50;
+char brief10;
+char brief20;
+char brief50;
 
 void setup() {
   Serial.begin(9600);
+  
   commando[0] = 'a';
   commando[1] = 'a';
   commando[2] = 'a';
-  pinMode(10, OUTPUT);
   
+  pinMode(10, OUTPUT);
  
-
-
-  while(!Serial)
-  {
-    
-  }
+  while(!Serial) {}
 }
 
 void stuurMotor10()
@@ -31,27 +26,20 @@ void stuurMotor10()
 
 
 void loop() {
-
   
-
-  if(Serial.available() > 2)
+ 
+  if(Serial.available() >= 3)
   {  
-    Serial.readBytes(commando, 3);
-
-    while(Serial.available() > 0)
-    {
-      Serial.read();
-    }
-    
-
+    Serial.readBytes(commando, 3);   
     
     brief10 = commando[0];
     brief20 = commando[1];
     brief50 = commando[2];
+
     Serial.print(brief10);
     Serial.print(brief20);
     Serial.println(brief50);
-
+    
     if(brief10 == '1')
     {
       stuurMotor10();
@@ -82,7 +70,11 @@ void loop() {
       stuurMotor10();
       stuurMotor10();
     }
+    delay(1);
+    Serial.read();
   }
- 
+  
+  
+  
   
 }
