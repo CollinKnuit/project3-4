e9f04aed52a1429aad596c91867110eb36862e64
Achraf Reyani <0980800@hr.nl>
2020-06-07 13:55:39 +0200

    geld dispenser code afgemaakt

--- code_voor_geld_dispenser/code_voor_geld_dispenser.ino
+++ code_voor_geld_dispenser/code_voor_geld_dispenser.ino
@@ -1,3 +1,5 @@
+//dit is de code van de geld dispenser
+
 char commando[3];
 char brief10;
 char brief20;
@@ -9,8 +11,10 @@ void setup() {
   commando[0] = 'a';
   commando[1] = 'a';
   commando[2] = 'a';
-  
+
+  pinMode(7, OUTPUT);
   pinMode(10, OUTPUT);
+  pinMode(13, OUTPUT);
  
   while(!Serial) {}
 }
@@ -20,6 +24,20 @@ void stuurMotor10()
   digitalWrite(10, HIGH);
   delay(2000);
   digitalWrite(10, LOW);
+  delay(100);
+}
+void stuurMotor20()
+{
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(100);
+}
+void stuurMotor50()
+{
+  digitalWrite(7, HIGH);
+  delay(2000);
+  digitalWrite(7, LOW);
   delay(100);
 }
 
@@ -69,6 +87,194 @@ void loop() {
       stuurMotor10();
       stuurMotor10();
       stuurMotor10();
+    }
+    if(brief10 == '6')
+    {
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+    }
+    if(brief10 == '7')
+    {
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+    }
+    if(brief10 == '8')
+    {
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+    }
+    if(brief10 == '9')
+    {
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+      stuurMotor10();
+    }
+
+    if(brief20 == '1')
+    {
+      stuurMotor20();
+    }
+    if(brief20 == '2')
+    {
+      stuurMotor20();
+      stuurMotor20();
+    }
+    if(brief20 == '3')
+    {
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+    }
+    if(brief20 == '4')
+    {
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+    }
+    if(brief20 == '5')
+    {
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+    }
+    if(brief20 == '6')
+    {
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+    }
+    if(brief20 == '7')
+    {
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+    }
+    if(brief20 == '8')
+    {
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+    }
+    if(brief20 == '9')
+    {
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+      stuurMotor20();
+    }
+
+    if(brief50 == '1')
+    {
+      stuurMotor50();
+    }
+    if(brief50 == '2')
+    {
+      stuurMotor50();
+      stuurMotor50();
+    }
+    if(brief50 == '3')
+    {
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+    }
+    if(brief50 == '4')
+    {
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+    }
+    if(brief50 == '5')
+    {
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+    }
+    if(brief50 == '6')
+    {
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+    }
+    if(brief50 == '7')
+    {
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+    }
+    if(brief50 == '8')
+    {
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+    }
+    if(brief50 == '9')
+    {
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
+      stuurMotor50();
     }
     delay(1);
     Serial.read();
